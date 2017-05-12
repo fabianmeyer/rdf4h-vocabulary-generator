950f54509da293f2246281ce21cd52a5273cff6e
Fabian Meyer <fmeyer@arago.de>
2017-05-12 14:19:47 +0200

    Added namespace generation and unode construction

--- src/Data/RDF/Vocabulary/Generator/VocabularyGenerator.hs
+++ src/Data/RDF/Vocabulary/Generator/VocabularyGenerator.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
-module Data.RDF.Vocabulary.Generator.VocabularyGenerator ( genVocabulary ) where
+module Data.RDF.Vocabulary.Generator.VocabularyGenerator (
+  genVocabulary
+) where
 
+import           ClassyPrelude
 import           Data.Char           (isLower)
-import           Data.List           (nub)
 import           Data.Maybe          (maybeToList)
-import           Data.RDF            (AdjHashMap, Node, Node (UNode), RDF, Rdf,
+import           Data.RDF            (AdjHashMap, Node, Node (UNode), unode, RDF, Rdf,
                                       TurtleParser (TurtleParser), parseFile,
-                                      subjectOf, triplesOf)
-import           Data.Text           (Text, cons, split, unpack)
+                                      subjectOf, triplesOf, prefixMappings, PrefixMappings(PrefixMappings))
 import qualified Data.Text           as T
+import qualified Data.Map as M
 import           Language.Haskell.TH
 
 genVocabulary :: String -> Q [Dec]
@@ -23,32 +26,55 @@ loadGraph file = parseFile (TurtleParser Nothing Nothing) file >>= \result -> ca
 
 vocabulary :: Rdf a => RDF a -> [Dec]
 vocabulary graph =
-  let subjects = nub $ subjectOf <$> triplesOf graph
-      iris     = extractIRIs subjects
-      names    = iriToName <$> iris
-      iriDecls = zipWith declareIRI names iris
-      irisDecl = declareIRIs names
-  in  irisDecl : iriDecls
-
-extractIRIs :: [Node] -> [Text]
-extractIRIs nodes = concat $ maybeToList . toIRI <$> nodes
+  let nameDecls = do
+        subject <- hashNub $ subjectOf <$> triplesOf graph
+        iri <- maybeToList $ toIRI subject
+        name <- maybeToList $ iriToName iri
+        return (name, declareIRI name iri)
+      (PrefixMappings prefixMappings') = prefixMappings graph
+      namespaceDecls = do
+        (prefix, iri) <- M.toList prefixMappings'
+        let name = mkName . unpack . escape $ prefix <> "NS"
+        return $ declarePrefix name prefix iri
+      iriDecls = snd <$> nameDecls
+      irisDecl = declareIRIs $ fst <$> nameDecls
+  in irisDecl : namespaceDecls <> iriDecls
 
 toIRI :: Node -> Maybe Text
 toIRI (UNode iri) = Just iri
 toIRI _           = Nothing
 
+packFun :: Exp
+packFun = VarE $ mkName "Data.Text.pack"
+
+unodeFun :: Exp
+unodeFun = VarE $ mkName "Data.RDF.Types.unode"
+
+mkPrefixedNSFun :: Exp
+mkPrefixedNSFun = VarE $ mkName "Data.RDF.Namespace.mkPrefixedNS"
+
 declareIRI :: Name -> Text -> Dec
 declareIRI name iri =
   let iriLiteral = LitE . StringL $ unpack iri
-  in  FunD name [Clause [] (NormalB iriLiteral) []]
+      unodeLiteral = AppE unodeFun $ AppE packFun iriLiteral
+  in  FunD name [Clause [] (NormalB unodeLiteral) []]
 
 declareIRIs :: [Name] -> Dec
 declareIRIs names =
   let iriList = ListE (VarE <$> names)
   in  FunD (mkName "iris") [Clause [] (NormalB iriList) []]
 
-iriToName :: Text -> Name
-iriToName = mkName . unpack . escape . last . filter (not . T.null) . split (`elem` separators)
+
+-- namespace = mkPrefixedNS "ogit" "http://www.purl.org/ogit/"
+declarePrefix :: Name -> Text -> Text -> Dec
+declarePrefix name prefix iri =
+  let prefixLiteral = AppE packFun . LitE . StringL . unpack $ prefix
+      iriLiteral = AppE packFun . LitE . StringL . unpack $ iri
+      namespace = AppE (AppE mkPrefixedNSFun prefixLiteral) iriLiteral
+  in  FunD name [Clause [] (NormalB namespace) []]
+
+iriToName :: Text -> Maybe Name
+iriToName iri = mkName . unpack . escape <$> (lastMay . filter (not . T.null) . T.split (`elem` separators)) iri
   where separators = ['/','#']
 
 escape :: Text -> Text
